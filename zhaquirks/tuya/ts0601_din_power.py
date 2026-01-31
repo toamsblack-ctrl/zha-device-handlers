5c472bf44af37e83c0d90d2eb61e5711f5b4e91b
toamsblack-ctrl <toamsblack@gmail.com>
2026-01-31 14:39:47 +0100

    Update ts0601_din_power.py
    Add models TZE204_goecjd1t

--- zhaquirks/tuya/ts0601_din_power.py
+++ zhaquirks/tuya/ts0601_din_power.py
@@ -240,6 +240,7 @@ class TuyaPowerMeter(TuyaSwitch):
         MODELS_INFO: [
             ("_TZE200_byzdayie", "TS0601"),
             ("_TZE200_ewxhg6o9", "TS0601"),
+            ("_TZE204_goecjd1t", "TS0601"),
         ],
         ENDPOINTS: {
             # <SimpleDescriptor endpoint=1 profile=260 device_type=51
